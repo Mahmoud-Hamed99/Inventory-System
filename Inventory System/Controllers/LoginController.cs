7d16e4b857517acf3f6d02665a4f18ea2cd9930a
mnoshy <mnoshy@offbeateg.com>
2021-04-11 12:19:22 +0200

    readyTest

--- Inventory System/Controllers/LoginController.cs
+++ Inventory System/Controllers/LoginController.cs
@@ -64,5 +64,56 @@ namespace Inventory_System.Controllers
 
             return View("Index");
         }
+        [VerifyUser]
+        public ActionResult Prof()
+        {
+            return View();
+        }
+        [VerifyUser]
+        [HttpPost]
+        public ActionResult Prof(string password)
+        {
+            try
+            {
+                var u = ((Inventory_System.Models.User)ViewBag.mainUser);
+                db.Users.Find(u.Id).Password = password;
+                db.SaveChanges();
+                Helper.Login(HttpContext, db, u.username, password, this);
+                switch (u.Roles)
+                {
+                    case "warehouse":
+                        return RedirectToAction("Index", "Items");
+                    //break;
+                    case "warehouseaudit":
+                        return RedirectToAction("Index", "ItemInputs", new { acc = true });
+                    //break;
+                    case "purchasing":
+                        return RedirectToAction("PurchasingApproval", "DemandItems");
+                    //break;
+                    case "generalaccountant":
+                        return RedirectToAction("Index", "BankAccounts");
+                    //break;
+                    case "demandplanning":
+                        return RedirectToAction("Index", "DemandItems");
+                    //break;
+                    case "projectplanning":
+                        return RedirectToAction("Index", "Projects");
+                    case "cost":
+                        return RedirectToAction("Index", "Items");
+                    case "safe":
+                        return RedirectToAction("Index", "Safes");
+                    //break;
+                    default:
+                        return RedirectToAction("Index", "Items");
+                        //break;
+                }
+            }
+            catch
+            {
+
+            }
+            
+            return View();
+        }
     }
 }
